abc319ce3af8abb8099851fc8ee8cfd578d3ff45
WarriorKozak <gm.tupota.valik@gmail.com>
2018-11-16 00:09:57 +0200

    add check

--- src/components/main.js
+++ src/components/main.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import './main.css'
 
+const checkedStyle = {
+  textDecoration: 'line-through'
+}
+
 export default class Main extends React.Component {
   input = React.createRef() // Bind input with component field
 	state = {
-    check: true,
+    value: '',
     tasks: []
   }
   
@@ -17,14 +21,15 @@ export default class Main extends React.Component {
   }
 
 	addTask = async () => {
-    await this.setState({tasks: this.state.tasks.concat(this.input.current.value)})
+    await this.setState({tasks: this.state.tasks.concat({ value: this.input.current.value, check: false })})
     localStorage.setItem('taskList', JSON.stringify(this.state.tasks))
+    this.input.current.value = this.state.value
 	}
 
   editTask = async (index) => {
-    const edited = prompt('', this.state.tasks[index])
+    const edited = prompt('', this.state.tasks[index].value)
     const tasks = this.state.tasks
-    tasks[index] = edited
+    tasks[index].value = edited
     
     await this.setState({ tasks })
 
@@ -33,55 +38,51 @@ export default class Main extends React.Component {
 
   deleteTask = async (index) => {
     const tasks = this.state.tasks
-    tasks.splice(index, 1)
+    tasks.splice(index.value, 1)
 
     await this.setState({ tasks })
     localStorage.setItem('taskList', JSON.stringify(this.state.tasks))
   }
 
-	zacherk = (e) => {
-		if(this.state.check) {
-			this.setState({check: !this.state.check})
-			e.target.style = `text-decoration: line-through;`
-		} else if (!this.state.check) {
-			this.setState({check: !this.state.check})
-			e.target.style = `text-decoration: underline;`
-		}
-		
+	setChecked = async (index) => {
+    const tasks = this.state.tasks
+    tasks[index].check = !tasks[index].check
+    await this.setState({ tasks })
+    localStorage.setItem('taskList', JSON.stringify(this.state.tasks))
 	}
 
   render(){
     return (
 			<div className = 'wrapper'>
-      <div>
-				<label htmlFor='task'> click</label>
-      	<input ref={this.input} id ='task' />
+        <div>
+          <div className='input'>
+            <label htmlFor='task'> click</label>
+            <input ref={this.input} id ='task' />
 
-				<button onClick = {this.addTask} >Add</button>
-			</div>
+            <button onClick = {this.addTask} >Add</button>
+          </div>
 
-				<br/>
-				<br/>
-				<div>
-					
-					<details open={true} onClick = {this.parser}>
-						<summary>
-							TASKS
-						</summary>
-          <ol>
-            {this.state.tasks.map((task, index) => (
-              <li key={index}>
-                <span onClick={this.zacherk}>{task}</span>
-                <button onClick={() => this.editTask(index)}>edit</button>
-                <button onClick={() => this.deleteTask(index)}>X</button>
-              </li>)
-            )}
-          </ol>
+          <div>
+          
+            <details open={true}>
+              <summary>
+                TASKS
+              </summary>
+            <ol>
+              {this.state.tasks.map((task, index) => (
+                <li key={index} >
+                  <span onClick={() => this.setChecked(index)} style={task.check ? checkedStyle : {}}>{task.value}</span>
+                  <button onClick={() => this.editTask(index)}>edit</button>
+                  <button onClick={() => this.deleteTask(index)}>delete</button>
+                </li>)
+              )}
+            </ol>
 
-					</details>
+            </details>
 
-					
-				</div>
+            
+          </div>
+        </div>
      </div>
 
     );
